f75bf218c1ba0a5308301e2d5cd1d1f088d1ce54
amberm291 <m.amber@iitg.ernet.in>
2016-11-08 01:32:18 +0530

    Added two pass k-means

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# SKMeans
-Implementation of spherical k-means
+## SKMeans
+
+Implementation of k-means with cosine distance as the distance metric. The computation of mean is still done in the same way as for standard k-means. Method `SKMeans` is used to compute `k` clusters for an input, based on cosine distances.
+
+### Requirements
+
+Requires `scipy` and `numpy` to execute. To install, follow the instruction on [this page](https://www.scipy.org/install.html).
+
+### Usage
+
+There is only one method `SKMeans` in the source file `skmeans.py`. To instantiate it, 
+
+```bash
+>>> from skmeans import SKMeans
+>>> no_clusters = 300
+>>> kmeans_inst = SKMeans(no_clusters,iters=15)
+```
+
+Here, `no_clusters` are the number of clustes to be generated and `iters` is the number of iterations for which k-means will be run. The default value of `iters` is set to 300.
+
+To run k-means on an input matrix `X`,
+
+```bash
+>>> X = numpy.random.rand((150,50))
+>>> kmeans_inst.fit(X)
+```
+
+`X` can be a sparse matrix or a numpy array. In this case, when no keyword arguments are provided, the centres are sampled randomly from `X`. To provide, your own centre values,
+
+```bash
+>>> centres = numpy.random.rand((10,50))
+>>> kmeans_inst.fit(X,sample=False,param_centres=centres)
+``` 
+
+Here `centres` can be a sparse matrix or a numpy 2d array. The default value of `param_centres` is `None`. If `sample` is set to `False`, `param_centres` should be prvided a matrix.
+
+In case, two pass k-means is to be used (In two pass kmeans, in the first pass, a small set of the input matrix is used to sample centres, then k-means is run over this small set of input and the centres. The new centres found from the first pass are then passed to the second pass of k-means, with the complete input matrix.), call `fit` method with the following parameter,
+
+```bash
+>>> kmeans_inst.fit(X,two_pass=True)
+```
+
+The default value of the flag `two_pass` is `False`. Setting it to `True`, it will take precedence over the `sample` flag and ignore it's value. The number of input samples for the first pass of k-means is determined by the following condition,
+
+```bash
+>>> no_samples = max(2*np.sqrt(X.shape[0]), 10*self.no_clusters)
+```
+
+### Distance Calculation
+
+`SKMeans` uses a matrix multiplication to calculate the cosine distances, hence, it is fairly fast in partice as compared to other methods, which use `scipy.spatial.distance.cdist` to compute the cosine distances.
+
+### Note
+
+This work is free. You can redistribute it and/or modify it under the terms of the Do Whatever You Want To Public License.
--- skmeans.py
+++ skmeans.py
@@ -6,27 +6,27 @@ from numpy.random import randint
 
 class SKMeans:
     def __init__(self, no_clusters, iters=300):
+        '''
+            Constructor for the class.
+
+            PARAMETERS:
+                no_clusters (int): The number of clusters to be generated.
+                iters (int): Number of iterations for the algorithm.
+        '''
         self.no_clusters = no_clusters
         self.iters = iters
 
-    def spherical_clustering(self, input_matrix):
-        sample_count = input_matrix.shape[1]
-        labels = np.zeros(sample_count, dtype='int8')
-        input_matrix = input_matrix/(norm(input_matrix,axis=1).reshape(sample_count,1))     
-        iters = 0
-        while iters < self.iters:
-            print iters
-            self.centres = self.centres/(norm(self.centres,axis=1).reshape(self.no_clusters,1))
-            clusdotprod = input_matrix.dot(self.centres.T)
-            for i in xrange(sample_count):
-                labels[i] = np.argmax(clusdotprod[i,:])            
-            clus_old = self.centres.copy()
-            for i in unique(labels):
-                self.centres[i,:] = mean(bnorm[labels==i], axis=0)
-            iters += 1
-
-
     def run_kmeans(self, input_matrix, delta=.001):
+        '''
+            The actual function performing kmeans.
+
+            PARAMETERS:
+                input_matrix (scipy.sparse or numpy.ndarray): Matrix containing input sample. It can either be a scipy sparse matrix or a numpy 2darray.
+                delta (float): Stopping criteria for k-means
+            
+            RETURNS:
+                None
+        '''
         input_samples, input_dimensions = input_matrix.shape
         no_centres, centre_dimensions = self.centres.shape
         if input_dimensions != centre_dimensions:
@@ -40,34 +40,107 @@ class SKMeans:
             self.labels = self.distances.argmax(axis=1)
             self.distances = np.ones(input_samples) - self.distances[input_seq,self.labels]
             avg_distance = self.distances.mean()
-            if (1 - delta) * prev_distance <= avg_distance <= prev_distance: break
+            try:
+                if (1 - delta) * prev_distance <= avg_distance <= prev_distance: break
+            except Exception, e:
+                continue
             prev_distance = avg_distance
-            print prev_distance
             for label in xrange(self.no_clusters):
                 indexes = np.where(self.labels == label)[0]
                 if len(indexes) > 0:
                     self.centres[label] = input_matrix[indexes].mean(axis=0)
 
-    def sample_centres(self, input_matrix):
-        sampleidx = random.sample(xrange(input_matrix.shape[0]), int(self.no_clusters))
-        return input_matrix[sampleidx]
+    def sample_centres(self, input_matrix, no_samples):
+        '''
+            Function to sample centres from the input matrix.
 
-    def fit(self, input_matrix, sample_centres=True, param_centres=None):
-        if sample_centres:
-            self.centres = input_matrix[randint(0,input_matrix.shape[0],self.no_clusters)]
+            PARAMETERS:
+                input_matrix (scipy.sparse or numpy.ndarray): Matrix containing input samples. It can either be a scipy sparse matrix or a numpy 2darray.
+                no_samples (int): The number of points to be sampled from input_matrix.
+
+            RETURNS:
+                A sparse matrix or numpy 2darray (depending upon input) with number of rows equal to no_samples and number of columns equal to column of input_matrix.
+        '''
+        return input_matrix[randint(0,input_matrix.shape[0],no_samples)]
+
+    def sample_kmeans(self, input_matrix):
+        '''
+            Two pass k-means to sample centres in the first pass by running k-means on a small input. The second pass uses these centres to compute the centres on the entire data.
+
+            PARAMETERS:
+                input_matrix (scipy.sparse or numpy.ndarray): Matrix containing input samples. It can either be a scipy sparse matrix or a numpy 2darray.
+
+            RETURNS:
+                None
+        '''
+        input_points, no_dimensions = input_matrix.shape
+        no_samples = max(2*np.sqrt(input_points), 10*self.no_clusters)
+        sampled_input = self.sample_centres(input_matrix, no_samples)
+        self.centres = self.sample_centres(input_matrix, self.no_clusters)
+        self.run_kmeans(sampled_input)
+        self.run_kmeans(input_matrix)
+
+    def fit(self, input_matrix, sample=True, param_centres=None, two_pass=False):
+        '''
+            Function to input the data and call run_kmeans on it.
+
+            PARAMETERS:
+                input_matrix (scipy.sparse or numpy.ndarray): Matrix containing input samples. It can either be a scipy sparse matrix or a numpy 2darray.
+                sample (boolean): By default set to True, this flag is used to sample centres from the input data. If set to False, a numpy array containing centre points should be passed to param_centres.
+                param_centres (scipy.sparse or numpy.ndarray): Is set to None by default. Should be passed a sparse matrix or numpy 2darray containing centre points, if sample is set to False.
+                two_pass (boolean): By default set to Flase, set this flag to True to execute a two pass k-means. If set to True, this flag takes precedence over the sample flag and ignores its value.
+
+            RETURNS: None
+
+        '''
+        input_matrix = input_matrix/norm(input_matrix,axis=1)
+
+        if two_pass:
+            self.sample_kmeans(input_matrix)
+            return
+
+        if sample:
+            self.centres = self.sample_centres(input_matrix, self.no_clusters)
         else:
             if not param_centres:
                 raise ValueError("Must provide centre matrix if sample_centres is set to False.")
-            self.centres = param_centres
+            self.centres = param_centres/norm(param_centres,axis=1)
         self.run_kmeans(input_matrix)
 
     def get_labels(self):
+        '''
+            Function to get cluster labels for each point in the input matrix.
+
+            PARAMETERS:
+                None 
+
+            RETURNS:
+                labels (list of ints): List containing labels for each point in the same order as they were passed in input matrix.
+        '''
         return self.labels
 
     def get_distances(self):
+        '''
+            Function to get distances for each point from their cluster centre in the input matrix.
+
+            PARAMETERS:
+                None 
+
+            RETURNS:
+                distances (np.array of type np.float64): Numpy array containing distances for each point in the same order as they were passed in input matrix.
+        '''
         return self.distances
 
     def get_centres(self):
+        '''
+            Function to get centre arrays for each cluster.
+
+            PARAMETERS:
+                None 
+
+            RETURNS:
+                centres (numpy.ndarray): Numpy 2darray where each represents the centre of a cluster.
+        '''
         return self.centres
 
 
